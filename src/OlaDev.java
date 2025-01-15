0747c7720de02d5f5b35014038e6984e27424183
alisson <alissonsantana16608@gmail.com>
2025-01-15 14:02:25 -0300

    Comentário

---
+++ src/OlaDev.java
@@ -0,0 +1,16 @@
+public class OlaDev {
+
+    /**
+     *Isso é um comentário <b>javadoc</b>
+     */
+
+    public static void main(String[] args) {
+        // Isso é um comentário de uma linha
+        /*
+            Isso é um comentário
+            de multiplas
+            linhas
+         */
+        System.out.println("KA-ME-HA-ME-HAAA");
+    }
+}
