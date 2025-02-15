561a4e3f8ce50d1a85349145d0cdfb20adea5e72
alisson <alissonsantana16608@gmail.com>
2025-02-15 11:53:34 -0300

    Arrays e os Tipos Primitivos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float e double 0
+        // char '\u0000' ' '
+        // String null
+
+        String [] idades = new String[3];
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+
+    }
+}
