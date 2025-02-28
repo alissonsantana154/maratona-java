07625cb5a1e8a8a7a791c82039fa3cb8563e11a3
alisson <alissonsantana16608@gmail.com>
2025-02-27 21:35:52 -0300

    Orientação Objetos Modificador de acesso private, get e set

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -5,12 +5,10 @@ import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Funciona
 public class FuncionarioTest01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
-        funcionario.nome = "Sanji";
-        funcionario.idade = 22;
-        funcionario.salarios = new double[]{1200, 987.32, 2000};
-
+        funcionario.setNome("Sanji");
+        funcionario.setIdade(22);
+        funcionario.setSalarios(new double[]{1200, 987.32, 2000});
         funcionario.imprime();
-
-
+        System.out.println("Média " + funcionario.getMedia());
     }
 }
