de740ee746c4186b61387b6b28f536238e4a80d2
alisson <alissonsantana16608@gmail.com>
2025-04-10 12:34:51 -0300

    Orientação Objetos - Modificador static

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/TestCarro01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.test;
+
+import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain.Carro;
+
+public class TestCarro01 {
+    public static void main(String[] args) {
+        Carro c1 = new Carro("BMW", 280);
+        Carro c2 = new Carro("Mercedes", 275);
+        Carro c3 = new Carro("Audi", 290);
+
+        Carro.velocidadeLimite = 180;
+
+        c1.imprimir();
+        c2.imprimir();
+        c3.imprimir();
+    }
+}
